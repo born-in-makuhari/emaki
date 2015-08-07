13744a59c31dad4523dba0330a65ce487f9fbb9f
craftpaperbag <kazu11518@gmail.com>
2015-08-08 02:06:39 +0900

    #6 views folder (.slim)

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'slim'
 
 group :test do
   gem 'rack'
--- emaki.rb
+++ emaki.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
 require 'sinatra'
+require 'slim'
 
 get '/' do
-  'emaki'
+  slim :index
 end

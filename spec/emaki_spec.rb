53311c678aec093ebf96c2358f1f587c499652fe
craftpaperbag <kazu11518@gmail.com>
2015-08-09 02:21:40 +0900

    #11 expect /new to return 200

--- spec/emaki_spec.rb
+++ spec/emaki_spec.rb
@@ -1,8 +1,14 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe 'Emaki' do
-  describe 'when GET /' do
+  describe 'GET /' do
     before { get '/' }
+    it 'returns 200' do
+      expect(last_response).to be_ok
+    end
+  end
+  describe 'GET /new' do
+    before { get '/new' }
     it 'returns 200' do
       expect(last_response).to be_ok
     end

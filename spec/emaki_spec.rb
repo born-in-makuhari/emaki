b50979e451d73b194190a5070b79a2057e52b200
craftpaperbag <kazu11518@gmail.com>
2015-08-09 02:42:29 +0900

    #11 spec red

--- spec/emaki_spec.rb
+++ spec/emaki_spec.rb
@@ -12,5 +12,32 @@ describe 'Emaki' do
     it 'returns 200' do
       expect(last_response).to be_ok
     end
+    it 'contains <form>' do
+      pending 'not yet'
+    end
+    it 'contains <input type="text" name="username">' do
+      pending 'not yet'
+    end
+    it 'contains <input type="text" name="slidename">' do
+      pending 'not yet'
+    end
+    it 'contains <input type="submit">' do
+      pending 'not yet'
+    end
+  end
+  describe 'POST /slides' do
+    before do
+      # TODO: refresh slides/ directory
+      post '/slides'
+    end
+    it 'redirects to /testuser/testslide' do
+      pending 'not yet'
+    end
+    it 'creates directory "slides/testuser/testslide"' do
+      pending 'not yet'
+    end
+    it 'creates png images in the directory' do
+      pending 'not yet'
+    end
   end
 end

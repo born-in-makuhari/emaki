a076b4686c52639169085f3f91376092aa937d54
craftpaperbag <kazu11518@gmail.com>
2015-08-09 12:03:51 +0900

    #11 spec green Slide.rmdir

--- spec/slide_spec.rb
+++ spec/slide_spec.rb
@@ -2,9 +2,14 @@ require File.expand_path '../spec_helper.rb', __FILE__
 
 # slide関連のファイル操作を行うSlideクラスをテスト
 describe 'Emaki::Slide' do
-  before do
-    @un_path = File.expand_path('../..', __FILE__) + "/slides/#{UN}/#{SN}"
+
+  # tips: allをつけると１回だけ実行する。
+  #       デフォルトはeach(毎回)
+  before :all do
+    @un_path = File.expand_path('../..', __FILE__) + "/slides/#{UN}"
     @sn_path = File.expand_path('../..', __FILE__) + "/slides/#{UN}/#{SN}"
+    puts "user  dir: #{@un_path}"
+    puts "slide dir: #{@sn_path}"
   end
 
   context "when (#{UN}, #{SN}) given" do
@@ -33,17 +38,16 @@ describe 'Emaki::Slide' do
 
     describe '.rmdir' do
       before do
-        FileUtils.rmdir(@sn_path)
-        FileUtils.rmdir(@un_path)
-        Slide.mkdir(@un, @sn)
+        FileUtils.mkdir_p(@sn_path)
+        Slide.rmdir(@un, @sn)
       end
 
-      after do
-        FileUtils.rmdir(@sn_path)
-        FileUtils.rmdir(@un_path)
-      end
-      # 注意：ユーザーディレクトリは削除しない
       it "(#{UN}, #{SN}) -> remove ..../slides/#{UN}/#{SN}" do
+        expect(FileTest.exist?(@sn_path)).to be false
+      end
+
+      it "'#{UN}' still exists" do
+        expect(FileTest.exist?(@un_path)).to be true
       end
     end
 

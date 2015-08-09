976b521d4e4fd7f53ae9724f9485e0a4d0cbc85f
craftpaperbag <kazu11518@gmail.com>
2015-08-09 11:56:03 +0900

    #11 spec green Slide.mkdir

--- spec/slide_spec.rb
+++ spec/slide_spec.rb
@@ -3,22 +3,34 @@ require File.expand_path '../spec_helper.rb', __FILE__
 # slide関連のファイル操作を行うSlideクラスをテスト
 describe 'Emaki::Slide' do
 
-  describe '.mkdir' do
-    context "(#{UN}, #{SN})" do
-      it "(#{UN}, #{SN})  -> create ..../slides/#{UN}/#{SN}"
+  context "when (#{UN}, #{SN}) given" do
+    before do
+      @sn_path = File.expand_path('../..', __FILE__) + "/slides/#{UN}/#{SN}"
+      @un_path = File.expand_path('../..', __FILE__) + "/slides/#{UN}/#{SN}"
+      puts @sn_path
+      puts @un_path
+      FileUtils.rmdir(@sn_path)
+      FileUtils.rmdir(@un_path)
+      Slide.mkdir(UN, SN)
     end
-  end
 
-  describe '.rmdir' do
-    #
-    # 注意：ユーザーディレクトリは削除しない
-    context "(#{UN}, #{SN})" do
+    after do
+      FileUtils.rmdir(@sn_path)
+      FileUtils.rmdir(@un_path)
+    end
+    describe '.mkdir' do
+      it "(#{UN}, #{SN})  -> create ..../slides/#{UN}/#{SN}" do
+        expect(FileTest.exist?(@un_path)).to be true
+        expect(FileTest.exist?(@sn_path)).to be true
+      end
+    end
+
+    describe '.rmdir' do
+      # 注意：ユーザーディレクトリは削除しない
       it "(#{UN}, #{SN}) -> remove ..../slides/#{UN}/#{SN}"
     end
-  end
 
-  describe '.makepath' do
-    context "(#{UN}, #{SN})" do
+    describe '.makepath' do
       it "(#{UN}, #{SN}) -> ..../slides/#{UN}/#{SN}"
     end
   end

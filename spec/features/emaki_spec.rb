070519aa3202b6422e3008df99e6771556b75fb5
craftpaperbag <kazu11518@gmail.com>
2015-08-17 07:29:21 +0900

    #38 + Feature Specs

---
+++ spec/features/emaki_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path '../../spec_helper.rb', __FILE__
+
+describe 'Common page', type: :feature do
+  it 'links to top' do
+    visit '/'
+    click_link 'toTop'
+    uri = URI.parse(current_url)
+    expect(uri.path).to eq '/'
+  end
+end

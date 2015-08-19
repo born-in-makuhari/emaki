f6059f5f447a288b5a143b4a84bfe115ddf70ad0
craftpaperbag <kazu11518@gmail.com>
2015-08-19 22:21:28 +0900

    #17 red ユーザー登録画面へのリンク

--- spec/features/emaki_spec.rb
+++ spec/features/emaki_spec.rb
@@ -1,11 +1,54 @@
 require File.expand_path '../../spec_helper.rb', __FILE__
 
-describe 'Common page', type: :feature do
-  it 'links to top' do
+# フィーチャースペック
+#
+# 画面とサーバーの機能を結合したスペック。
+# 画面表示や画面操作のテスト。
+#
+# TODO: 今spec/emaki_spec.rbに書いているフィーチャースペックはココに移す
+
+describe 'Top page', type: :feature do
+  before do
     visit '/'
+  end
+
+  it 'links to Top' do
     click_link 'toTop'
     uri = URI.parse(current_url)
     expect(uri.path).to eq '/'
+  end
+
+  context 'if not signed in, ' do
+    it 'links to Register' do
+      click_link 'toRegister'
+      uri = URI.parse(current_url)
+      expect(uri.path).to eq '/register'
+    end
+  end
+end
+
+describe 'Register page', type: :feature do
+  before { visit '/register' }
+
+  it 'links to Top' do
+    click_link 'toTop'
+    uri = URI.parse(current_url)
+    expect(uri.path).to eq '/'
+  end
+
+  context 'if not signed in' do
+    it 'has form #register'
+    it 'has form action="POST /users"'
+    it 'has input #username'
+    it 'has input #name'
+    it 'has input #email'
+    it 'has input #password'
+    it 'has submit button'
+  end
+
+  context 'if signed in' do
+    it 'redirects to Top'
+    it 'has attention "welcome"'
   end
 end
 

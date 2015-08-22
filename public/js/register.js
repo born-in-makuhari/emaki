1b2607c34b187e48302560c4def0b8eef6f8a82d
craftpaperbag <kazu11518@gmail.com>
2015-08-22 19:20:57 +0900

    fix

--- public/js/register.js
+++ public/js/register.js
@@ -12,9 +12,13 @@ $(function () {
     // usernameが手入力されていなければ自動補完する
     if ( ! Register.UN_CHANGED ) {
       var slug = $('#register #email').val();
+
+      // @より前を抜き出す
       var slug = slug.replace(/@.+/g, '');
+      // 使えない文字を変換する
       var slug = slug.replace(/\./g, '-');
       var slug = slug.replace(/[0-9]/g, '');
+      var slug = slug.replace(/[^a-zA-Z-_]/g, '');
       var slug = slug.replace(/[-_]+$/, '');
       var slug = slug.replace(/^[-_]+/, '');
       // 重複排除

acec61e88c0f987065e9f60c3a8c31b03553bc3e
craftpaperbag <kazu11518@gmail.com>
2015-09-01 19:02:53 +0900

    use same port 12321 when 'dock/quick-start.sh' without 'random'

--- dock/quick-start.sh
+++ dock/quick-start.sh
@@ -1,4 +1,10 @@
-docker run -dti -P --name emaki -v $PWD:/srv/emaki emaki bash
+# 引数がrandomならランダムにポート割り当て
+if [ $1 -a $1 = "random" ]; then
+  docker run -dti -P --name emaki -v $PWD:/srv/emaki emaki bash
+else
+  docker run -dti -p 12321:80 --name emaki -v $PWD:/srv/emaki emaki bash
+fi
+
 docker exec emaki /bin/bash -c /srv/emaki/dock/compile.sh
 docker exec emaki /bin/bash -c /srv/emaki/dock/start.sh
 echo
